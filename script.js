4a8ccb82222c0c3a073d2d545bc9c87f3f997531
MadziaSzmatloch <98941475+MadziaSzmatloch@users.noreply.github.com>
2025-04-10 18:20:23 +0200

    work in progress

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ const countryInput = document.getElementById('country');
 const myForm = document.getElementById('form');
 const modal = document.getElementById('form-feedback-modal');
 const clicksInfo = document.getElementById('click-count');
+let countryChoices;
+const vatUECheckbox = document.getElementById('vatUE');
+const invoiceSection = document.getElementById('invoiceSection');
 
 function handleClick() {
     clickCount++;
     clicksInfo.innerText = clickCount;
 }
 
-async function fetchAndFillCountries() {
+/* async function fetchAndFillCountries() {
     try {
         const response = await fetch('https://restcountries.com/v3.1/all');
         if (!response.ok) {
@@ -22,14 +25,37 @@ async function fetchAndFillCountries() {
     } catch (error) {
         console.error('Wystąpił błąd:', error);
     }
-}
+}  */
+
+    async function fetchAndFillCountries() {
+        try {
+            const response = await fetch('https://restcountries.com/v3.1/all');
+            if (!response.ok) throw new Error('Błąd pobierania danych');
+            const data = await response.json();
+            const countries = data.map(c => c.name.common).sort();
+    
+            countryInput.innerHTML = `<option value="">Wybierz kraj</option>` + countries.map(country =>
+                `<option value="${country}">${country}</option>`).join('');
+    
+            countryChoices = new Choices(countryInput, {
+                searchEnabled: true,
+                shouldSort: true,
+                itemSelectText: '',
+                placeholderValue: 'Wybierz kraj',
+            });
+        } catch (error) {
+            console.error('Wystąpił błąd:', error);
+        }
+    }
+    
 
 function getCountryByIP() {
     fetch('https://get.geojs.io/v1/ip/geo.json')
         .then(response => response.json())
         .then(data => {
             const country = data.country;
-            // TODO inject country to form and call getCountryCode(country) function
+            countryInput.value = country;
+            getCountryCode(country);
         })
         .catch(error => {
             console.error('Błąd pobierania danych z serwera GeoJS:', error);
@@ -48,17 +74,63 @@ function getCountryCode(countryName) {
     })
     .then(data => {        
         const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
-        // TODO inject countryCode to form
+        const codeSelect = document.getElementById('countryCode');
+    for (let i = 0; i < codeSelect.options.length; i++) {
+        if (codeSelect.options[i].value === countryCode) {
+            codeSelect.selectedIndex = i;
+            break;
+        }
+    }
     })
     .catch(error => {
         console.error('Wystąpił błąd:', error);
     });
 }
 
+async function fetchAndFillCountryCodes() {
+    try {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        if (!response.ok) throw new Error('Błąd pobierania kodów');
+
+        const data = await response.json();
+        const countryCodes = data
+            .filter(c => c.idd?.root && c.idd?.suffixes?.length)
+            .map(c => {
+                const code = c.idd.root + c.idd.suffixes[0];
+                const name = c.name.common;
+                return { name, code };
+            })
+            .sort((a, b) => a.name.localeCompare(b.name));
+
+        const codeSelect = document.getElementById('countryCode');
+        codeSelect.innerHTML = `<option selected>Wybierz...</option>` + countryCodes
+            .map(({ name, code }) => `<option value="${code}">${code} (${name})</option>`)
+            .join('');
+    } catch (error) {
+        console.error('Błąd pobierania kodów kierunkowych:', error);
+    }
+}
+
+
 
 (() => {
     // nasłuchiwania na zdarzenie kliknięcia myszką
     document.addEventListener('click', handleClick);
 
-    fetchAndFillCountries();
+    
+    fetchAndFillCountryCodes();
+    fetchAndFillCountries().then(() => getCountryByIP());
+
+    countryInput.addEventListener('change', () => {
+        const selectedCountry = countryInput.value;
+        getCountryCode(selectedCountry);
+    });
+
+    vatUECheckbox.addEventListener('change', () => {
+        if (vatUECheckbox.checked) {
+            invoiceSection.removeAttribute('hidden');
+        } else {
+            invoiceSection.setAttribute('hidden', 'true');
+        }
+    });
 })()
